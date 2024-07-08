91e75e43ba665b1aedd6e336a489cee0297570ec
Gabriel <gabriel1007@yahoo.com>
2024-07-08 18:59:06 -0300

    Created train code

--- main.py
+++ main.py
@@ -1,28 +1,8 @@
-import tiktoken
-
-encoding = tiktoken.encoding_for_model("gpt2")
-
-encoded = encoding.encode("Hello")
-
-print(encoded, encoding.decode(encoded))
-
-
-
-
-import sys; sys.exit(0)
-from bpe import BPE, merge
-import numpy as np
-
-""" bpe = BPE()
-bpe.train_bpe(100, 'taylorswift.txt') """
-
-""" bpe = BPE()
-print(bpe.decode(bpe.encode('hello world'))) """
-
-
 from bpe import BPE
 
 bpe = BPE()
-bpe.train_bpe(100, 'taylorswift.txt')
+bpe.train_bpe(1000, 'Eisenhorn.txt')
 
+""" import numpy as np  
 
+print(np.load('vocab.npy', allow_pickle=True).item()) """
